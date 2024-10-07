0228e136175aa5f37e4785ab827735e1292f1046
Jan Dolejsi <JDolejsi@slb.com>
2024-10-08 00:48:42 +0200

    share instead of shareAll

--- remote_app_shell/webpack.config.js
+++ remote_app_shell/webpack.config.js
@@ -1,4 +1,4 @@
-const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+const { share, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 
 module.exports = withModuleFederationPlugin({
 
@@ -6,8 +6,48 @@ module.exports = withModuleFederationPlugin({
     "mfe1": "http://localhost:4221/remoteEntry.js",    
   },
 
-  shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
-  },
+  shared: share({
+    '@angular/core': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    '@angular/common': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    '@angular/common/http': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    // the @angular/forms is missing in the RDK app shell till October 2024
+    // '@angular/forms': {
+    //   singleton: true,
+    //   strictVersion: true,
+    //   requiredVersion: 'auto'
+    // },     
+    '@angular/router': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    rxjs: {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    '@angular/material': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    },
+    // '@angular/material-moment-adapter': {
+    //   singleton: true,
+    //   strictVersion: true,
+    //   requiredVersion: 'auto',
+    // },
+  }),
 
 });
